d879cb8b0687f068c53199beb6f2787949db8fdc
JakoPapaFrita <jacobouribe@inemjose.edu.co>
2021-03-06 22:37:05 -0500

    subiendo primera entrega

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Frase from './components/Frase';
+
+function App() {
+  return (
+    <div className="App">
+      <Frase />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Frase.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import "./frase.css";
+import Logo from './bblogo.png'
+
+const Frase = () => {
+
+    const [fraseActivo, setFraseActivo] = useState(false);
+    const [frase, setFrase] = useState({
+        authorBB : '',
+        quoteBB : ''
+    });
+
+    const buscarFrase = async() => {
+        setFraseActivo(true);
+        let response = await fetch('http://breaking-bad-quotes.herokuapp.com/v1/quotes')
+        response = await response.json();
+        const {author, quote} = response[0];
+        setFrase({
+            authorBB : author ,
+            quoteBB : quote
+        })
+    }
+
+    const { authorBB, quoteBB } = frase;
+    return ( 
+        <div className="frase-container d-flex justify-content-center align-items-center">
+            <div className="d-flex justify-content-center align-items-center flex-column container text-center">
+                <img src={Logo} alt="logo" className="logo"/>
+                <div className="mt-5 botton-buscarFrase" onClick={buscarFrase}>
+                    Buscar frase
+                </div>
+                {
+                    fraseActivo ? (
+                        <div className="container-quote">
+                            <div className="frase">
+                                "{ quoteBB }"
+                            </div>
+                            <div className="author d-flex justify-content-end">
+                                -{ authorBB }
+                            </div>
+                        </div>
+                    ) : null
+                }
+            </div>
+        </div>
+     );
+}
+ 
+export default Frase;
